52bc2aafb26b71f7bf08b18f45d7914e642f6407
Syed Ali Razza <tech@arqen.studio>
2026-06-18 00:50:53 +0500

    Increase reservation button font size minimum on mobile

--- src/pages/home/IntroSection.tsx
+++ src/pages/home/IntroSection.tsx
@@ -60,7 +60,7 @@ const IntroSection = () => {
             href="https://www.sevenrooms.com/explore/kajabynuma/reservations/create/search/"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ width: "clamp(295px, 28vw, 650px)", height: "clamp(44px, 4.5vw, 90px)", gap: "8px", paddingLeft: "48px", paddingRight: "48px", fontSize: "clamp(12px, 1.1vw, 22px)" }}
+            style={{ width: "clamp(295px, 28vw, 650px)", height: "clamp(44px, 4.5vw, 90px)", gap: "8px", paddingLeft: "48px", paddingRight: "48px", fontSize: "clamp(16px, 1.1vw, 22px)" }}
             className="bg-[#D6B283] dark:bg-[#32341D] text-[#32341D] dark:text-[#D6B283] border border-[#D6B283] dark:border-[#32341D] flex items-center justify-center hover:opacity-80 transition-opacity font-moche font-normal tracking-widest whitespace-nowrap"
           >
             MAKE A RESERVATION
