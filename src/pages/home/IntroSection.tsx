4dcdc9ffe04b5b731b16a4cfa9344c3d2e4b6b4b
Syed Ali Razza <tech@arqen.studio>
2026-06-17 19:56:01 +0500

    Scale up reservation button more on large screens

--- src/pages/home/IntroSection.tsx
+++ src/pages/home/IntroSection.tsx
@@ -60,7 +60,7 @@ const IntroSection = () => {
             href="https://www.sevenrooms.com/explore/kajabynuma/reservations/create/search/"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ width: "clamp(295px, 22vw, 500px)", height: "clamp(44px, 3.5vw, 70px)", gap: "8px", paddingLeft: "48px", paddingRight: "48px" }}
+            style={{ width: "clamp(295px, 28vw, 650px)", height: "clamp(44px, 4.5vw, 90px)", gap: "8px", paddingLeft: "48px", paddingRight: "48px" }}
             className="bg-[#D6B283] text-[#32341D] border border-[#D6B283] flex items-center justify-center hover:opacity-80 transition-opacity font-moche font-normal text-sm tracking-widest whitespace-nowrap"
           >
             MAKE A RESERVATION
